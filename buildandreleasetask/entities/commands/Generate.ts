6e902524e99a7e9d793899892e1d5f648dd3ab02
Max Klevebrand <max.klevebrand@gmail.com>
2025-03-29 11:14:07 +0100

    Display errors better

--- buildandreleasetask/entities/commands/Generate.ts
+++ buildandreleasetask/entities/commands/Generate.ts
@@ -73,11 +73,9 @@ function Generate() {
     exec(command, { cwd: workingDirectory}, (error: Error | null, stdout: string, stderr: string) => {
         if (error) {
             console.error(`exec error: ${error}`);
-            return;
         }
         if (stderr) {
             console.error(`stderr: ${stderr}`);
-            return;
         }
         console.log(`Aspirate successfully generated manifest: ${stdout}`);
     });
